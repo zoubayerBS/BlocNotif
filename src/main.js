250762186f66c32ddf3801472d79552cd821fee7
ZOUBAYER BENSAID <zoubayerbensaid@ZOUBAYERs-MacBook-Air.local>
2026-04-27 03:25:31 +0100

    Simplify OneSignal init for production

--- src/main.js
+++ src/main.js
@@ -18,27 +18,8 @@ if ('serviceWorker' in navigator) {
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 OneSignalDeferred.push(async function(OneSignal) {
   await OneSignal.init({
-    appId: import.meta.env.VITE_ONESIGNAL_APP_ID || "63bad8ac-eb43-46fd-809b-061962bc04f2",
-    safari_web_id: "web.onesignal.auto.63f03f56-6218-498c-8507-6f654924a686", 
-    notifyButton: {
-      enable: true,
-      position: 'bottom-left',
-      size: 'medium',
-      colors: {
-        'circle.background': '#4f46e5'
-      }
-    },
-    allowLocalhostAsSecureOrigin: true,
-    welcomeNotification: {
-      "title": "BlocNotif",
-      "message": "Merci de vous être abonné !"
-    }
+    appId: "63bad8ac-eb43-46fd-809b-061962bc04f2",
   });
-
-  // Display prompt automatically if not subscribed
-  if (!OneSignal.Notifications.permission) {
-    OneSignal.Slidedown.showSlidedownPrompt();
-  }
 });
 
 export default app
